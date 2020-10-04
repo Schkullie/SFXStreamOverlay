87034a50f04e0e7eaed794e93f9e3322629a4cf6
Schkullie <71150548+Schkullie@users.noreply.github.com>
2020-10-04 22:05:02 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,24 @@
+
+var channel = "Schkullie" ;
+//your Twitchname. Leave the "" alone. It will not work without them.
+
+var commandname = "test";
+//the name of your command
+
+var time = 4;
+//the time the image is shown in seconds
+
+var commandtimeout = 2 ;
+//set a timeout for the command usage, so I can't be spammed
+
+var imagename = "image";
+//name of your imagefile. Leave the "" alone. It will not work without them.
+
+var imagefiletype = "png";
+//filetype of your imagefile, like png, jpg, gif. Leave the "" alone. It will not work without them.
+
+var soundname = "sound";
+//name of your soundfile. Leave the "" alone. It will not work without them.
+
+var soundfiletype = "mp3";
+//filetype of your soundfile, like mp3, ogg, wav. Leave the "" alone. It will not work without them.
